e5a719074dcc95dbec1f5718c1d8aadb0aeffbd5
CXwudi <cx2010017@gmail.com>
2020-08-20 21:08:12 -0400

    :hankey: be ready for no jackson annotation

--- common-vocaloid-entity/src/test/java/mikufan/cx/common_vocaloid_util/jackson/ObjMapperUtil.java
+++ common-vocaloid-entity/src/test/java/mikufan/cx/common_vocaloid_util/jackson/ObjMapperUtil.java
@@ -1,11 +1,15 @@
 package mikufan.cx.common_vocaloid_util.jackson;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.cfg.MapperBuilder;
 import com.fasterxml.jackson.datatype.eclipsecollections.EclipseCollectionsModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import lombok.NonNull;
 
 
@@ -19,15 +23,22 @@ abstract class ObjMapperUtil {
     return (M) mapper.enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
   }
 
-  protected static <M extends ObjectMapper, B extends MapperBuilder<M,B>> M addDefaultFlags(MapperBuilder<M, B> builder){
-    return builder.addModule(new EclipseCollectionsModule())
+  protected static <M extends ObjectMapper, B extends MapperBuilder<M,B>> M addDefaultFlagsForReadWrite(B builder){
+    return addDefaultFlags(builder)
         .enable(SerializationFeature.INDENT_OUTPUT)
         .serializationInclusion(JsonInclude.Include.NON_NULL)
         .build();
   }
 
-  protected static <M extends ObjectMapper, B extends MapperBuilder<M,B>> M addDefaultFlagsForReadOnly(MapperBuilder<M, B> builder){
-    return builder.addModule(new EclipseCollectionsModule())
+  protected static <M extends ObjectMapper, B extends MapperBuilder<M,B>> M addDefaultFlagsForReadOnly(B builder){
+    return addDefaultFlags(builder)
         .build();
+  }
+
+  protected static <M extends ObjectMapper, B extends MapperBuilder<M,B>> B addDefaultFlags(B builder){
+    return builder
+        .addModule(new EclipseCollectionsModule())
+        .addModule(new ParameterNamesModule())
+        .visibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
   }
 }
